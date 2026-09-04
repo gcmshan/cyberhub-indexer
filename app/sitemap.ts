4a93846438b7c3efcc2a01e00d9f77bca10e19e7
gcmshan <gcmthe2shan@gmail.com>
2026-09-04 19:02:22 +0530

    Fix static export for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,14 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static'; // <--- මෙන්න මේ පේළිය එක් කරන්න
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://cyberhub.site',
+      url: 'https://cyberhub-indexer.pages.dev',
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 1.0,
+      priority: 1,
     },
   ];
 }
